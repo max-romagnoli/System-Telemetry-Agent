1484c2b63a55d7601189962ffc8c255d3057035e
max-romagnoli <massimiliano.romagnoli02@gmail.com>
2024-02-12 17:34:36 +0000

    Abstract metrics into a metrics.py file

--- exporter/collectors/cpu.py
+++ exporter/collectors/cpu.py
@@ -21,9 +21,18 @@ class CPUCollector:
         TODO: @
         Returns the current CPU frequency in MHz.
         """
+        pass
 
     def get_temperature(self):
         """
         TODO: @
         Returns the CPU temperature in degrees Celsius.        
         """
+        pass
+
+    def __str__(self) -> str:
+        """
+        TODO:
+        Returns to string representation of all CPU metrics.        
+        """
+        pass
--- exporter/exporter.py
+++ exporter/exporter.py
@@ -1,14 +1,25 @@
 from prometheus_client import start_http_server, Gauge
 import time
 
+from .metrics import *
+from .collectors.cpu import CPUCollector
+
+
+cpu_collector = CPUCollector()
+
 
 def export_metrics(port=8000):
+
+    """
+    TODO:
+    Starts an http server on port 8000 and enters a loop to collect metrics and export them to Prometheus.
+    """
 
     start_http_server(port)
     print(f"Exporter running on port {port}")
 
     while True:
-        
-        # TODO: collect metrics
+
+        cpu_utilization_gauge.set(cpu_collector.get_utilization())
 
         time.sleep(5)
