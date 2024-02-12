1db83e9c8a05a412ea93cf38aff30b763443451e
max-romagnoli <massimiliano.romagnoli02@gmail.com>
2024-02-12 17:31:41 +0000

    Add CPU collector class

--- exporter/collectors/cpu.py
+++ exporter/collectors/cpu.py
@@ -1,1 +1,29 @@
-# TODO:
+import psutil
+
+class CPUCollector:
+
+    def __init__(self) -> None:
+        """
+        TODO: @
+        Constructor
+        """
+        pass
+
+    def get_utilization(self):
+        """
+        TODO: @
+        Returns the CPU utilization as a percentage.
+        """
+        pass
+
+    def get_frequency(self):
+        """
+        TODO: @
+        Returns the current CPU frequency in MHz.
+        """
+
+    def get_temperature(self):
+        """
+        TODO: @
+        Returns the CPU temperature in degrees Celsius.        
+        """
