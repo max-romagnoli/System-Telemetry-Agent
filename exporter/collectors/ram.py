c426bc03e50dd5a3252fc5314d179b7a78a9a36b
root <ljdzed>
2024-02-13 01:58:52 +0000

    Setup Finished and added 3 files with remainings statistics inplace.

---
+++ exporter/collectors/ram.py
@@ -0,0 +1,31 @@
+import psutil
+
+class ramCollector:
+
+    def __init__(self) -> None:
+        """
+        TODO: @
+        Constructor
+        """
+        pass
+
+    def get_utilization(self):
+        """
+        TODO: @
+        Returns the RAM utilization as a percentage.
+        """
+        pass
+
+    def get_memory(self):
+        """
+        TODO: @
+        Returns the total memory installed.
+        """
+        pass
+
+    def __str__(self) -> str:
+        """
+        TODO:
+        Returns to string representation of all RAM metrics.        
+        """
+        pass
