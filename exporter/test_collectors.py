41de1c06180cbee2b3c22e1e308e8c531067561e
QimingNie <123891635+QimingNie@users.noreply.github.com>
2024-02-14 20:32:12 +0000

    Create test_collectors.py
    Create pytest

---
+++ exporter/test_collectors.py
@@ -0,0 +1,11 @@
+# Here are tests about functions from collectors,Add tests if needed.
+import psutil
+from cpu import get_frequency
+
+def test_cpu_get_frequency():
+    try:
+        with open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq', 'r') as f:
+            frequency = int(f.read()) / 1000  
+        assert get_frequency(self) == frequency
+    except Exception as e:
+        print(f"Error reading CPU frequency: {e}")
