d31b530f50626c9bb7ac4f5d48e91e097b027e88
QimingNie <123891635+QimingNie@users.noreply.github.com>
2024-02-15 02:00:09 +0000

    Delete exporter/test_collectors.py

--- exporter/test_collectors.py
+++
@@ -1,6 +0,0 @@
-# Here are tests about functions from collectors,Add tests if needed.
-import psutil
-from collectors.cpu import get_frequency
-
-def test_cpu_get_frequency():
-    assert isinstance(frequency, (int, float)) #Tests whether to return a number.
