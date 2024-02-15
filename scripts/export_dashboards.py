fd4c9b63c740fb82a388853898d324d852742581
max-romagnoli <massimiliano.romagnoli02@gmail.com>
2024-02-15 12:09:12 +0000

    Added script to automatically refresh local files with Grafana changes

---
+++ scripts/export_dashboards.py
@@ -0,0 +1,78 @@
+import os
+from dotenv import load_dotenv
+import requests
+import json
+import time
+
+load_dotenv()
+
+grafana_api_key = os.getenv('GRAFANA_SYNC_API')
+grafana_url = os.getenv('GRAFANA_LOCAL_API_URI')
+
+dashboards_directory = "../grafana/conf/provisioning/dashboards"
+
+
+def fetch_dashboards(api_key, grafana_url):
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+    response = requests.get(f'{grafana_url}/search', headers=headers)
+    return response.json()
+
+
+def fetch_dashboard_details(api_key, grafana_url, dashboard_uid):
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+    response = requests.get(f'{grafana_url}/dashboards/uid/{dashboard_uid}', headers=headers)
+    return response.json()
+
+
+def files_are_different(existing_file, new_data):
+    if os.path.exists(existing_file):
+        with open(existing_file, 'r') as file:
+            existing_data = json.load(file)
+        
+        existing_data.pop('version', None)
+        new_data.pop('version', None)
+        
+        return existing_data != new_data
+    else: # file does not exist: new dashboard
+        return True
+
+
+def sync_json_files(dashboard_details, file_path):
+    dashboard_data = dashboard_details.get('dashboard', {})
+    if files_are_different(file_path, dashboard_data):
+        with open(file_path, 'w') as file:
+            json.dump(dashboard_data, file, indent=4)
+            print(f"Dashboard '{file_path}' updated with new changes.")
+
+
+def export_dashboards():
+
+    dashboards = fetch_dashboards(grafana_api_key, grafana_url)
+
+    for dashboard in dashboards:
+        dashboard_title = dashboard['title']
+        dashboard_uid = dashboard['uid']
+        dashboard_details = fetch_dashboard_details(grafana_api_key, grafana_url, dashboard_uid)
+        file_path = f"{dashboards_directory}/{dashboard_title}.json"
+        sync_json_files(dashboard_details, file_path)
+
+
+def main():
+    
+    if grafana_api_key is None:
+        raise ValueError("No Grafana API Key found. Please check your .env file.")
+    
+    print("Syncing local JSON files to Grafana...")
+    print("Refresh rate: 2s")
+    
+    while True:
+        export_dashboards()
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
