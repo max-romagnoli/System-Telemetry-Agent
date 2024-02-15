cfd2202fdfa2de3eefce1036ecbadefa6bac28bf
QimingNie <123891635+QimingNie@users.noreply.github.com>
2024-02-15 01:41:43 +0000

    Update test_collectors.py

--- exporter/test_collectors.py
+++ exporter/test_collectors.py
@@ -1,6 +1,6 @@
 # Here are tests about functions from collectors,Add tests if needed.
 import psutil
-from cpu import get_frequency
+from collectors.cpu import get_frequency
 
 def test_cpu_get_frequency():
     try:
