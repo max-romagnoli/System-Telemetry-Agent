1f15ac6fe262ca7f8f01e34e95eae2a37df0d56d
QimingNie <123891635+QimingNie@users.noreply.github.com>
2024-02-15 02:14:18 +0000

    Update test_collectors.py

--- exporter/collectors/test_collectors.py
+++ exporter/collectors/test_collectors.py
@@ -1,6 +1,7 @@
 # Here are tests about functions from collectors,Add tests if needed.
 import psutil
-from .cpu import CPUCollector.get_frequency
+from .cpu import CPUCollector
 
 def test_cpu_get_frequency():
+    frequency = CPUCollector.get_frequency()
     assert isinstance(frequency, (int, float)) #Tests whether to return a number.
