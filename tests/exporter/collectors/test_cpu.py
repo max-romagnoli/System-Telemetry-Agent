6b85dd28df7acd92a050acf7b35e86283fe4b80a
QimingNie <123891635+QimingNie@users.noreply.github.com>
2024-02-16 16:29:38 +0000

    Update test_cpu.py

--- tests/exporter/collectors/test_cpu.py
+++ tests/exporter/collectors/test_cpu.py
@@ -1,8 +1,6 @@
 # Here are tests about functions from collectors,Add tests if needed.
 import psutil
-import sys
-sys.path.append('/home/runner/work/System-Telemetry-Agent/System-Telemetry-Agent/exporter/collectors/cpu.py')
-from .cpu import CPUCollector
+from ../exporter/collectors/cpu import CPUCollector
 
 def test_cpu_get_frequency():
     frequency = CPUCollector.get_frequency()
