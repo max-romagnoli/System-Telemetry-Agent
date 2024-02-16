5192f4d96c98f578a5476511580fcf68a5b7ba0a
QimingNie <123891635+QimingNie@users.noreply.github.com>
2024-02-16 16:34:07 +0000

    Update test_cpu.py

--- tests/exporter/collectors/test_cpu.py
+++ tests/exporter/collectors/test_cpu.py
@@ -1,6 +1,6 @@
 # Here are tests about functions from collectors,Add tests if needed.
 import psutil
-from ... import CPUCollector
+from ...cpu import CPUCollector
 
 def test_cpu_get_frequency():
     frequency = CPUCollector.get_frequency()
