e7cdec48fedaad510e8df561ef40d1a89b901b74
l3331l4 <adilr@tcd.ie>
2024-02-19 23:16:43 +0000

    Added call to methods and tests, updated prometheus config to read metrics

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -24,4 +24,7 @@ def export_metrics(port=8000):
 
         cpu_utilization_gauge.set(cpu_collector.get_utilization())
 
+        ram_utilization_gauge.set(ram_collector.get_utilization())
+        ram_memory_gauge.set(ram_collector.get_memory())
+
         time.sleep(5)
--- exporter/metrics.py
+++ exporter/metrics.py
@@ -3,3 +3,6 @@ from prometheus_client import Gauge
 cpu_utilization_gauge = Gauge("cpu_utilization", "CPU utilization percentage")
 cpu_frequency_gauge = Gauge("cpu_frequency", "CPU frequency in MHz")
 cpu_temperature_gauge = Gauge("cpu_temperature", "CPU temperature in Celsius")
+
+ram_utilization_gauge = Gauge("ram_utilization", "RAM utilization percentage")
+ram_memory_gauge = Gauge("ram_memory", "Total RAM memory installed")
