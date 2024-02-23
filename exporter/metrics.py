8c8ae757bc2cc65c800f20f0fec807316a8e6c0c
l3331l4 <adilr@tcd.ie>
2024-02-23 16:24:55 +0000

    Improved gauge descriptions

--- exporter/metrics.py
+++ exporter/metrics.py
@@ -4,5 +4,5 @@ cpu_utilization_gauge = Gauge("cpu_utilization", "CPU utilization percentage")
 cpu_frequency_gauge = Gauge("cpu_frequency", "CPU frequency in MHz")
 cpu_temperature_gauge = Gauge("cpu_temperature", "CPU temperature in Celsius")
 
-ram_utilization_gauge = Gauge("ram_utilization", "RAM utilization percentage")
-ram_memory_gauge = Gauge("ram_memory", "Total RAM memory installed")
+ram_utilization_gauge = Gauge("stagent_ram_utilization_percentage", "Total RAM utilization as a percentage")
+ram_memory_gauge = Gauge("stagent_ram_memory_bytes", "Total RAM memory installed in bytes")
