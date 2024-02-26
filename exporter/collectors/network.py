92024a8d8bdcffabb895bac3f21e6dcb94e5e912
cindy ariyo <ariyoc@tcd.ie>
2024-02-26 22:50:38 +0000

    Added tests and edited prometheus config

--- exporter/collectors/network.py
+++ exporter/collectors/network.py
@@ -8,18 +8,26 @@ class NetworkCollector:
         Constructor
         """
     
+    
+    def get_unit(self, bytes):
+        """
+        Returns bytes sent and received in Mb.
+        """
+        bits = bytes * 8
+        megabits = bits /1000000
+        return megabits
+    
 
     def get_traffic_in(self):
         """
-        @cindyariyo
         Returns the inbound traffic in Mb/s.
         """
         traffic_in = psutil.net_io_counters()
         return self.get_unit(traffic_in.bytes_recv) 
+    
 
     def get_traffic_out(self):
         """
-        @cindyariyo
         Returns the outbound traffic in Mb/s.
         """
         traffic_out = psutil.net_io_counters(nowrap=True)   
@@ -28,23 +36,8 @@ class NetworkCollector:
 
     def __str__(self) -> str:
         """
-        @cindyariyo
         Returns to string representation of all networking metrics.        
         """
         traffic_in = self.get_traffic_in()
         traffic_out = self.get_traffic_out()
         return f"Inbound Traffic: {traffic_in} Mb/s, Outbound Traffic: {traffic_out} Mb/s"
-
-
-    """
-    Helper methods        
-    """
-
-    def get_unit(self, bytes):
-        """
-        @cindyariyo
-        TODO: Missing description        
-        """
-        bits = bytes * 8
-        megabits = bits /1000000
-        return megabits
