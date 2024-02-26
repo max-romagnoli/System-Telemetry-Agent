f8b3ca4a36dab51362c7b1b1e7616bfe05522a52
cindy ariyo <ariyoc@tcd.ie>
2024-02-26 22:10:30 +0000

    Added network tests, network data working

--- tests/exporter/collectors/test_network.py
+++ tests/exporter/collectors/test_network.py
@@ -1,28 +1,39 @@
-
 import unittest
 from exporter.collectors.network import NetworkCollector
 
 class TestNetworkCollector(unittest.TestCase):
 
+    def test_get_unit(self):
+        """
+        Test if bytes are coverted to Mb.
+        """
+        bytes = 345214
+        mb = (bytes * 8) / 1000000
+        network_collector = NetworkCollector()
+        units = network_collector.get_unit(bytes)
+        self.assertEqual(units, mb)
+
     def test_get_traffic_in(self):
         """
-        Test if bytes are received and returned in Mb/s
+        Test if bytes are received and returned in Mb/s.
         """
         network_collector = NetworkCollector()
         traffic_in = network_collector.get_traffic_in()
         self.assertIsInstance(traffic_in, float)
+        self.assertTrue(traffic_in >= 0)
     
     def test_get_traffic_out(self):
         """
-        Test if bytes are sent and returned in Mb/s
+        Test if bytes are sent and returned in Mb/s.
         """
         network_collector = NetworkCollector()
         traffic_out = network_collector.get_traffic_out()
         self.assertIsInstance(traffic_out, float)
+        self.assertTrue(traffic_out >= 0)
 
     def test__str__(self):
         """
-        Test if a string representation of all networking metrics is returned
+        Test if a string representation of all networking metrics is returned.
         """
         network_collector = NetworkCollector()
         traffic_in = network_collector.get_traffic_in()
