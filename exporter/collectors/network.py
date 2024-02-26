7ecc71627d3ea36a3eeee54742a4e45c383e8a21
max-romagnoli <massimiliano.romagnoli02@gmail.com>
2024-02-26 14:31:35 +0000

    Moved helper method

--- exporter/collectors/network.py
+++ exporter/collectors/network.py
@@ -8,15 +8,10 @@ class NetworkCollector:
         Constructor
         """
     
-    
-    def get_unit(self, bytes):
-        bits = bytes * 8
-        megabits = bits /1000000
-        return megabits
-    
 
     def get_traffic_in(self):
         """
+        @cindyariyo
         Returns the inbound traffic in Mb/s.
         """
         traffic_in = psutil.net_io_counters()
@@ -24,6 +19,7 @@ class NetworkCollector:
 
     def get_traffic_out(self):
         """
+        @cindyariyo
         Returns the outbound traffic in Mb/s.
         """
         traffic_out = psutil.net_io_counters(nowrap=True)   
@@ -32,8 +28,19 @@ class NetworkCollector:
 
     def __str__(self) -> str:
         """
+        @cindyariyo
         Returns to string representation of all networking metrics.        
         """
         traffic_in = self.get_traffic_in()
         traffic_out = self.get_traffic_out()
         return f"Inbound Traffic: {traffic_in} Mb/s, Outbound Traffic: {traffic_out} Mb/s"
+
+
+    """
+    Helper methods        
+    """
+
+    def get_unit(self, bytes):
+        bits = bytes * 8
+        megabits = bits /1000000
+        return megabits
