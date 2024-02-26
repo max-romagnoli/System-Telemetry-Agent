f4698fc41fa522520ad43450625df98d62802953
mccooeyc11 <127849325+mccooeyc11@users.noreply.github.com>
2024-02-26 15:50:18 +0000

    Wrote tests and rudimentary CPU temperature method

--- exporter/collectors/cpu.py
+++ exporter/collectors/cpu.py
@@ -11,31 +11,32 @@ class CPUCollector:
 
     def get_utilization(self):
         """
-        TODO: @
+        @mccooeyc11
         Returns the CPU utilization as a percentage.
         """
-        return psutil.cpu_percent();
+        return psutil.cpu_percent()
 
     def get_frequency(self):
         """
-        TODO: @
+        @mccooeyc11
         Returns the current CPU frequency in MHz.
         """
-        return psutil.cpu_freq()[0];
+        return psutil.cpu_freq()[0]
 
     def get_temperature(self):
         """
-        TODO: @
+        TODO: @mccooeyc11
         Returns the CPU temperature in degrees Celsius.        
         """
+        return psutil.sensors_temperatures()
         pass
 
     def __str__(self) -> str:
         """
-        TODO:
+        @mccooeyc11
         Returns to string representation of all CPU metrics.        
         """
-        utilization = self.get_utilization();
-        frequency = self.get_frequency();
-        temp = self.get_temperature();
-        return "CPU utilization: {utilization}%\n CPU frequency: {frequency}MHz\n CPU temperature: {temp}°C\n";
+        utilization = self.get_utilization()
+        frequency = self.get_frequency()
+        temp = self.get_temperature()
+        return "CPU utilization: {utilization}%\n CPU frequency: {frequency}MHz\n CPU temperature: {temp}°C\n"
---
+++ tests/exporter/collectors/test_cpu.py
@@ -0,0 +1,42 @@
+import unittest
+from exporter.collectors.cpu import CPUCollector
+
+class TestCPUCollector(unittest.TestCase):
+
+    def test_get_utilization(self):
+        """
+        @mccooeyc11
+        Test whether CPU utilization is within an acceptable range (0-100%)
+        """
+        cpu_collector = CPUCollector()
+        utilization = cpu_collector.get_utilization()
+        self.assertIsInstance(utilization, float)
+        self.assertTrue(0.0 <= utilization <= 100.0)
+
+    def test_get_freq(self):
+        """
+        @mccooeyc11
+        Test whether CPU frequency is a positive number
+        """
+        cpu_collector = CPUCollector()
+        freq = cpu_collector.get_frequency()
+        self.assertIsInstance(freq, float)
+        self.assertTrue(freq > 0.0)
+
+        
+
+    def test_str(self):
+        """
+        @mccooeyc11
+        Test whether the string representation of CPU metrics is correct
+        """
+        cpu_collector = CPUCollector()
+        utilization = cpu_collector.get_utilization()
+        frequency = cpu_collector.get_frequency()
+        temp = cpu_collector.get_temperature()
+        expected_output = f"CPU utilization: {utilization}%\n CPU frequency: {frequency}MHz\n CPU temperature: {temp}°C\n"
+        self.assertEqual(str(cpu_collector), expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
