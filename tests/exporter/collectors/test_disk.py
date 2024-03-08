8ce79bd944f997189ad8c724abacfafd67e42ba6
Victor D'Alessandris <dalessav@tcd.ie>
2024-03-08 21:14:15 +0100

    Library wasnt working so added class declaration

--- tests/exporter/collectors/test_disk.py
+++ tests/exporter/collectors/test_disk.py
@@ -1,39 +1,45 @@
 # Here are tests about functions from collectors,Add tests if needed.
 from exporter.collectors.disk import DiskCollector
+import unittest
 
+class TestDiskCollector(unittest.TestCase):
+    
+    
+    def test_disk_get_utilization(self):
+        """
+        Tests if a percentage as a number is returned
+        """
+        usage = DiskCollector.get_utilization()
+        self.assertIsInstance(usage, (int, float)) #
+        self.assertTrue(0 <= usage <= 0)
 
-def test_disk_get_utilization(self):
-    """
-    Tests if a percentage as a number is returned.
-    """
-    usage = DiskCollector.get_utilization()
-    assert isinstance(usage, (int, float)) #
-    assert (0 <= usage <= 0)
+    def test_disk_get_total_space(self):
+        """
+        Tests if a number is returned.
+        """
+        usage = DiskCollector.get_total_space()
+        self.assertIsInstance(usage, (int, float)) #
 
-def test_disk_get_total_space():
-    """
-    Tests if a number is returned.
-    """
-    usage = DiskCollector.get_total_space()
-    assert isinstance(usage, (int, float)) #
+    def test_disk_get_reads(self):
+        """
+        Tests if a number is returned.
+        """
+        reads = DiskCollector.get_reads()
+        self.assertIsInstance(reads, (int, float)) #
 
-def test_disk_get_reads():
-    """
-    Tests if a number is returned.
-    """
-    reads = DiskCollector.get_reads()
-    assert isinstance(reads, (int, float)) #
+    def test_disk_get_writes(self):
+        """
+        Tests if a number is returned.
+        """
+        writes = DiskCollector.get_writes()
+        self.assertIsInstance(writes, (int, float)) #
 
-def test_disk_get_writes():
-    """
-    Tests if a number is returned.
-    """
-    writes = DiskCollector.get_writes()
-    assert isinstance(writes, (int, float)) #
+    def test_disk__str__(self):
+        """
+        Tests if a string is returned.
+        """
+        string = DiskCollector.__str__()
+        self.assertIsInstance(string, (str)) #
 
-def test_disk__str__():
-    """
-    Tests if a string is returned.
-    """
-    string = DiskCollector.__str__()
-    assert isinstance(string, (str)) #
+if __name__ == '__main__':
+    unittest.main()
